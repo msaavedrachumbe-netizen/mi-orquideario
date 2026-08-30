a95c28083cf848c16c4426b013bb02a052d2e34a
msaavedrachumbe-netizen <msaavedrachumbe@gmail.com>
2026-08-30 16:01:49 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mi-orquideario-v1-v2';
+const CACHE='mi-orquideario-v3-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
